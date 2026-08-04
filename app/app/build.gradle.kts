adfd023f585f9de329e15d1dcfe2776473a4f886
curlyman98 <185939841+curlyman98@users.noreply.github.com>
2026-08-05 01:57:23 +0200

    Implement initial app screen and side menu

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -38,4 +38,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+    implementation("androidx.drawerlayout:drawerlayout:1.2.0")
 }
--- app/app/src/main/java/com/example/chess_clock_app/MainActivity.kt
+++ app/app/src/main/java/com/example/chess_clock_app/MainActivity.kt
@@ -3,10 +3,23 @@ package com.example.chess_clock_app
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+
+import android.widget.ImageButton
+
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+
+        val menuButton = findViewById<ImageButton>(R.id.menuButton)
+
+        menuButton.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
     }
 }
